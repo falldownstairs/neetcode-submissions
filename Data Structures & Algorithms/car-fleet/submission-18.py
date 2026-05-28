2c871f6bbc4b258cc5eb12857ee464b47d1e6f69
Eric Chen <105571226+falldownstairs@users.noreply.github.com>
2026-05-28 15:33:48 -0700

    Add: car-fleet - submission-18

---
+++ Data Structures & Algorithms/car-fleet/submission-18.py
@@ -0,0 +1,21 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        res = len(position)
+        cars = [[position[i],speed[i]] for i in range(len(position))]
+        cars = sorted(cars, key = lambda x: x[0])
+        print(cars)
+        
+        front_pos = cars[-1][0]
+        front_speed = cars[-1][1]
+        for i in range(len(cars)-2,-1,-1):
+            print(i)
+            if (target - cars[i][0])/cars[i][1] <= (target - front_pos)/front_speed:
+                res -= 1
+                print('a')
+            else:
+                front_pos = cars[i][0]
+                front_speed = cars[i][1]
+                print('b')
+            
+
+        return res
