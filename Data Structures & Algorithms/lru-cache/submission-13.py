feedc1a133e8d5024e83c38666d33710d7dc2300
Eric Chen <105571226+falldownstairs@users.noreply.github.com>
2026-06-27 14:56:07 -0700

    Add: lru-cache - submission-13

---
+++ Data Structures & Algorithms/lru-cache/submission-13.py
@@ -0,0 +1,30 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.hashmap = {}
+        self.usage = []
+
+    def get(self, key: int) -> int:
+        for i in range(len(self.usage)):
+            if self.usage[i] == key:
+                self.usage.pop(i)
+                break
+        self.usage.append(key)
+        return self.hashmap.get(key, -1)
+
+    def put(self, key: int, value: int) -> None:
+        print('b', self.hashmap, self.usage)
+        if len(self.hashmap) == self.capacity and key not in self.hashmap.keys():
+            for i in range(len(self.usage)):
+                if self.usage[i] in self.hashmap.keys():
+                    self.hashmap.pop(self.usage[i])
+                    break
+        self.hashmap[key] = value
+
+        for i in range(len(self.usage)):
+            if self.usage[i] == key:
+                self.usage.pop(i)
+                break
+        self.usage.append(key)
+        print('a', self.hashmap, self.usage)
