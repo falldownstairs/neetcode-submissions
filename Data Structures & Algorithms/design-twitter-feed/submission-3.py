4006e249ddd6bb6e4e88a98d52c487a955ad0641
Eric Chen <105571226+falldownstairs@users.noreply.github.com>
2026-07-08 19:48:11 -0700

    Add: design-twitter-feed - submission-3

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-3.py
@@ -0,0 +1,34 @@
+class Twitter:
+
+    def __init__(self):
+        self.accounts = {}
+        self.time = 0
+        self.tweets = []
+        self.tweetUsers = {}
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweets.append(tweetId)
+        self.tweetUsers[tweetId] = userId
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        res = []
+        i = 1
+        if userId not in self.accounts.keys():
+            self.accounts[userId] = set([userId])
+        print(self.tweets)
+        print(self.accounts)
+        print(self.tweetUsers)
+        while len(res) < 10 and i <= len(self.tweets):
+            if self.tweetUsers[self.tweets[-i]] in self.accounts[userId]:
+                res.append(self.tweets[-i])
+            i+=1
+        return res
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId not in self.accounts.keys():
+            self.accounts[followerId] = set([followerId])
+        self.accounts[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followerId != followeeId and followerId in self.accounts.keys() and followeeId in self.accounts[followerId]:
+            self.accounts[followerId].remove(followeeId)
